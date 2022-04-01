b79962c02a99045c66b8fc60dee6c8a043e32fe6
Emma Fattori <emma@applydigital.co>
2022-04-01 09:28:21 -0400

    setting up state

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [people, setPeople] = useState([{
+    name: 'Amanda Batula', 
+    url: '', 
+    age: 30, 
+    note: 'Most annoying cast member'
+  }, {
+    name: 'Paige DeSorbo', 
+    url: '', 
+    age: 30, 
+    note: 'Best dressed cast member'
+  }])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>People invited to our party</h1>
     </div>
   );
 }
